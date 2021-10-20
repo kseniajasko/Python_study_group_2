936b19aebd386e75fc490beaf826fb0062d911eb
kseniajasko@gmail.com <kseniajasko555>
2021-10-20 22:57:33 +0300

    added changes hw_30_08

--- hw_30_08/task2.py
+++ hw_30_08/task2.py
@@ -6,10 +6,18 @@ s_name = 'Oksana'
 
 n = len(s_name)
 
-def factorial(n: int)-> int:
-    if n == 0:
-        return 1
-    return n*factorial(n-1)
+factorial = 1
 
-print(f'{n}! = {factorial(n)}')
+while n > 0:
+    factorial *= n
+    n -= 1
+
+print(f'{len(s_name)}! = {factorial}')
+
+# def factorial(n: int)-> int:
+#     if n == 0:
+#         return 1
+#     return n*factorial(n-1)
+#
+# print(f'{n}! = {factorial(n)}')
 
