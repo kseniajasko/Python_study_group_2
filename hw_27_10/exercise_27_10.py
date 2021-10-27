4618ea7f450a8a5a792e4884f816069192ca79e6
kseniajasko@gmail.com <kseniajasko555>
2021-10-27 21:32:08 +0300

    added exercise hw_27_10

---
+++ hw_27_10/exercise_27_10.py
@@ -0,0 +1,67 @@
+#1. Відсортувати тварин за віком
+animals = [
+    {'type': 'penguin', 'name': 'Stephanie', 'age': 8},
+    {'type': 'elephant', 'name': 'Devon', 'age': 3},
+    {'type': 'puma', 'name': 'Moe', 'age': 5},
+]
+
+animals_sorted = sorted(animals, key = lambda i: i['age'])
+#print(animals_sorted)
+
+#2. З використанням list comprehension cтворити список квадратів непарних елементів вхідного списку numbers
+numbers = [4, 2, 1, 6, 9, 7]
+
+list_square = [i*i for i in numbers if i%2]
+#print(list_square)
+
+#3. Реалізувати функцію послідовного пошуку, який шукає потрібний елемент, починаючи з кінця вхідного списку
+names = ['Joe', 'Mary', 'Ann', 'Andrew', 'Stephan', 'Rosie']
+element_to_find = 'Stephan'
+
+def find_reverse(element, list):
+    index = len(list) - 1
+    while index > 0:
+        if list[index] == element:
+            return True
+        else:
+            index -= 1
+
+# print(find_reverse(element_to_find, names))
+
+
+'''
+4. Реалізувати клас Container, який відповідає наступним умовам:
+   - кожен об'єкт цього класу має атрибут elements, який при створенні нового об'єкту класу є пустим списком
+   - об'єкт має метод add(), який додає новий елемент до списку
+   - тип елементів, що можуть зберігатися у контейнері, визначається за першим доданим елементом: якщо першим 
+   було додано int, всі наступні мають бути int. Якщо першим було додано str, всі наступні також str. 
+   Модливі типи даних - примітивні типи мови Python (без колекцій та мапінгів)
+   Якщо ця умова не виконується - метод add повертає помилку TypeError
+   - elements завжди відсортовано за зростанням
+'''
+
+class Container:
+
+    def __init__(self):
+        self.elements = []
+
+    def add(self, el):
+        if len(self.elements) == 0:
+            self.elements.append(el)
+        else:
+            if type(el) == type(self.elements[0]):
+                self.elements.append(el)
+            else:
+                raise TypeError("Anothere type element")
+
+    def __str__(self):
+        sorted_list = sorted(self.elements)
+        return f'{sorted_list}'
+
+
+
+# container_1 = Container()
+# container_1.add('aszbb')
+# container_1.add('cszhd')
+# container_1.add('bshshh')
+# print(container_1)
